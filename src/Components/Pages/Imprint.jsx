ba87261c2cf511d91219ca8b553b74cf1e8a3fe8
54m1r <43683232+54m1r@users.noreply.github.com>
2024-08-30 11:51:59 +0200

    added imprint

---
+++ src/Components/Pages/Imprint.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Link } from "react-router-dom";
+import { error404 } from "../../constant/images";
+import Footer from "../Footer";
+import { Header } from "../../Components/HeaderNew";
+
+const ImprintPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="container bg-cover bg-no-repeat bg-center">
+        <div className="mx-auto flex flex-col">
+          <h2 style={{marginTop: 10}} className="mb-6">Impressum</h2>
+          <div className="mb-10">
+            <div className="bg-cover bg-no-repeat bg-center">
+              <p>
+                medex GmbH
+                <br />
+                Donaustra&szlig;e 64
+                <br />
+                87700 Memmingen
+              </p>
+              <p>
+                Handelsregister: HRB 21020
+                <br />
+                Registergericht: Amtsgericht Memmingen
+              </p>
+              <p>
+                <strong>Vertreten durch:</strong>
+                <br />
+                Marius Zoll
+              </p>
+              <p
+                style={{
+                  fontWeight: 600,
+                  fontSize: 22,
+                  marginTop: 15,
+                  marginBottom: 10,
+                }}
+              >
+                Kontakt
+              </p>
+              <p>
+                Telefon: 015127150688
+                <br />
+                E-Mail: info@medex-consulting.de
+              </p>
+              <p
+                style={{
+                  fontWeight: 600,
+                  fontSize: 22,
+                  marginTop: 15,
+                  marginBottom: 10,
+                }}
+              >
+                Umsatzsteuer-ID
+              </p>
+              <p>
+                Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a
+                Umsatzsteuergesetz:
+                <br />
+                DE367414432
+              </p>
+              <p
+                style={{
+                  fontWeight: 600,
+                  fontSize: 22,
+                  marginTop: 15,
+                  marginBottom: 10,
+                }}
+              >
+                Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+              </p>
+              <p>
+                Wir sind nicht bereit oder verpflichtet, an
+                Streitbeilegungsverfahren vor einer
+                Verbraucherschlichtungsstelle teilzunehmen.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default ImprintPage;
